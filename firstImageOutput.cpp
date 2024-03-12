c92e44930c9a2c4109707888caf33de9d4277e88
erin stone <erinliannestone@gmail.com>
2024-03-12 13:51:49 -0700

    progress bar

--- firstImageOutput.cpp
+++ firstImageOutput.cpp
@@ -21,6 +21,7 @@ int main() {
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n"; //ppm header
 
     for(int j = 0; j < image_height; ++j) {
+        std::clog << "\rscanlines remaining: " << (image_height - j) << ' ' << std::flush; //progress indicator log
         for(int i = 0; i < image_width; ++i) {
             auto r = double(i) / (image_width - 1);
             auto g = double(j) / (image_height-1);
@@ -33,4 +34,5 @@ int main() {
             std::cout << ir << ' ' << ig << ' ' << ib << '\n'; //each pixel
         }
     }
+    std::clog << "\rdone!                  \n";
 }
