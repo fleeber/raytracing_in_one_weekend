11799b027c83129c6cbb87d33bc1f72f6538d325
erin stone <erinliannestone@gmail.com>
2024-03-12 17:55:36 -0700

    more header files and world hittable using same normal coloring

---
+++ hittable_list.h
@@ -0,0 +1,53 @@
+#ifndef HITTABLE_LIST_H
+#define HITTABLE_LIST_H
+
+#include "hittable.h"
+
+#include <memory> //shared_ptr
+#include <vector>
+
+using std::shared_ptr; //lets you omit std::
+using std::make_shared;
+
+//C++ INSTANCING TIP
+//shared_ptr<type> is a pointer to an allocated type with reference counting
+//each time you assign its value to another shared_ptr the reference count increments
+//when shared_ptrs go out of scope (end of block/function) it decrements
+//object is safely deleted when counter reaches zero
+//example allocation: auto vec3_ptr = make_shared<vec3>(1.345239, 4.427340, 5.239743);
+//shared_ptrs allow multiple geometries to share a common instance
+//e.g. spheres that use the same material
+
+//std::vector is a generic array-like collection of things that automatically grows
+//as more values are added. push_back adds a value to the end of the vector member variable objects
+
+class hittable_list : public hittable { //stores a list of hittables
+    public:
+        std::vector<shared_ptr<hittable>>objects;
+        
+        hittable_list() {}
+        hittable_list(shared_ptr<hittable> object) {add(object);}
+        
+        void clear() {objects.clear();}
+
+        void add(shared_ptr<hittable> object) {
+            objects.push_back(object);
+        }
+
+        bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override {
+            hit_record temp_rec;
+            bool hit_anything = false;
+            auto closest_so_far = ray_tmax;
+
+            for(const auto& object : objects) {
+                if(object->hit(r, ray_tmin, closest_so_far, temp_rec)) {
+                    hit_anything = true;
+                    closest_so_far = temp_rec.t;
+                    rec = temp_rec;
+                }
+            }
+            return hit_anything;
+        }
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
+#include "rtweekend.h"
+
 #include "color.h"
-#include "vec3.h"
-#include "ray.h"
+#include "hittable.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
 #include <iostream>
 
@@ -18,42 +21,16 @@ r g b
 
 */
 
-double hit_sphere(const point3& center, double radius, const ray& r) {
-    //given a (centered at origin) sphere, radius r, and a point (x,y,z) ON the sphere,
-    //x*x + y*y + z*z = r*r
-    //                <      point is inside sphere
-    //                >      point is outside sphere
-    //given an arbitrarily positioned sphere at (Cx, Cy, Cz)
-    //(x-Cx)(x-Cx) + (y-Cy)(y-Cy) + (z-Cz)(z-Cz) = r*r    (Cx => C_x  not C*x)
-    //vector from center C to point P(x,y,z) is (P-C)
-    //(P-C) dot (P-C) = (x-Cx)(x-Cx) + (y-Cy)(y-Cy) + (z-Cz)(z-Cz)
-    //equivalently (P-C) dot (P-C) = r*r
-    //a point P that satisfies the above equation is on the sphere.
-    //  a vector dotted with itself == the squared length of the vector
-    vec3 oc = r.origin() - center;
-    auto a = r.direction().length_squared();
-    auto half_b = dot(oc, r.direction());
-    auto c = oc.length_squared() - radius*radius;
-    auto discriminant = half_b*half_b - a*c;
-    
-    //positive discriminant = ray passes in and out at 2 points on sphere
-    //negative discriminant = ray never touches sphere
-    //zero discriminant = ray touches sphere once tangentially
-    if(discriminant < 0) {
-        return -1.0;
-    } else { //find the location of the hit
-        return (-half_b - sqrt(discriminant)) / a; //right now it's less of a ray and more of a line
-    }
-}
 
-color ray_color(const ray& r) {
-    auto t = hit_sphere(point3(0,0,-1), 0.5, r); //define a sphere
-    if(t > 0.0) { //then render the sphere
-        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
+
+color ray_color(const ray& r, const hittable& world) {
+    hit_record rec;
+    if(world.hit(r, 0, infinity, rec)) { //then render the hittable
         //each normal is the vector from the center to the surface point, where the ray origin is moved to the surface and normalized
-        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+        //or the opposite when it gets flipped inside out
+        return 0.5 * (rec.normal + color(1,1,1)); //normal sphere
     }
-
+    //if it didn't hit, make a sky gradient
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
     //linear interpolation = (1 - a) * startValue + a * endValue   where 0.0<=a<=1.0
@@ -70,11 +47,16 @@ int main() {
     int image_height = static_cast<int>(image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;
 
+    // world
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
+
     //viewport aspect ratio may differ from aspect_ratio so we use calculated image aspect ratio to determine final viewport width
     auto viewport_height = 2.0; //arbitrary value
     auto viewport_width = viewport_height * (static_cast<double>(image_width) / image_height);
 
-    //camera
+    // camera
     auto focal_length = 1.0;
     auto camera_center = point3(0,0,0);
 
@@ -91,7 +73,7 @@ int main() {
     auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
 
-    //render
+    // render
 
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n"; //ppm header
 
@@ -101,7 +83,7 @@ int main() {
             auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
             auto ray_direction = pixel_center - camera_center; //didn't normalize to improve speed?
             ray r(camera_center, ray_direction);
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
             write_color(std::cout, pixel_color);
         }
     }
